d652acedf76db1cf3793152d7b6ab1ff964c56a0
Pedro Sarmento <phsp@cin.ufpe.br>
2025-03-18 22:38:30 -0300

    feat: adicionar malha de triângulos e materiais para objetos 3D

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from camera import Camera
 from color import Color
 import numpy as np
 import cv2
+from mesh import TriangleMesh
 
 def main():
    
-    camera_position = Point(0, 0, 0)
-    camera_look_at = Point(0, 0, -1)
+    camera_position = Point(0, 0, 20)
+    camera_look_at = Point(0, 0, 0)
     camera_up_vector = Vector(0, 1, 0)
     camera_distance = 1
     camera_h_res = 800
@@ -22,12 +23,20 @@ def main():
 
     image = np.zeros((camera_v_res, camera_h_res, 3), dtype=np.uint8)
 
+    obj_reader = ObjReader("./inputs/icosahedron/icosahedron.obj")
+    mesh = TriangleMesh(
+        vertices=obj_reader.get_vertices(),
+        faces=[face.vertice_indices for face in obj_reader.get_faces()],
+        normals=obj_reader.normals,
+        color=Color(0, 255, 0)
+    )
+
     # Lista de objetos
     objects = [
-        Sphere(Point(0, 0, -5), 1, Color(255, 0, 0)),
-        Sphere(Point(-2, 1, -6), 1.2, Color(0, 255, 0)),
-        Sphere(Point(2, -1, -4), 0.8, Color(0, 0, 255)),
-        Plane(Point(0, -1, 0), Vector(0, 1, 0), Color(255, 255, 0)) 
+        mesh,
+        #Sphere(Point(-2, 1, -6), 1.2, Color(0, 255, 0)),
+        #Sphere(Point(2, -1, -4), 0.8, Color(0, 0, 255)),
+        #Plane(Point(0, -1, 0), Vector(0, 1, 0), Color(255, 255, 0)) 
     ]
  
     # Testando interseções
@@ -44,6 +53,7 @@ def main():
                     color = obj.color.array()
             
             image[j, i] = color
+
 
     cv2.imshow("Ray Casting - Interseção com Esferas, Planos e Malhas de triângulos", image)
     cv2.waitKey(0)
---
+++ mesh.py
@@ -0,0 +1,48 @@
+from color import Color
+
+class TriangleMesh:
+    def __init__(self, vertices, faces, normals, color):
+        self.vertices = vertices 
+        self.faces = faces
+        self.normals = [Vector(n.x, n.y, n.z) for n in normals]
+        self.color = color
+        self.n_triangles = len(faces)
+        self.n_vertices = len(vertices)
+    
+    def intersect(self, ray):
+        closest_t = float('inf')
+        hit = False
+
+        print(f"Faces {self.n_triangles}:")
+        
+        for face in self.faces:
+            print(f"rodou uma face")
+
+            v0, v1, v2 = [self.vertices[i] for i in face]
+            edge1 = v1 - v0
+            edge2 = v2 - v0
+            h = ray.direction.cross(edge2)
+            a = edge1.dot(h)
+            
+            if -1e-6 < a < 1e-6:
+                continue  # O raio é paralelo ao triângulo
+            
+            f = 1.0 / a
+            s = ray.origin - v0
+            u = f * s.dot(h)
+            
+            if u < 0.0 or u > 1.0:
+                continue
+            
+            q = s.cross(edge1)
+            v = f * ray.direction.dot(q)
+            
+            if v < 0.0 or u + v > 1.0:
+                continue
+            
+            t = f * edge2.dot(q)
+            if t > 1e-6 and t < closest_t:
+                closest_t = t
+                hit = True
+        
+        return closest_t if hit else None
